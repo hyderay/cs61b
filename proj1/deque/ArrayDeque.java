c9453d2b06b99efbe89ed3cb21afeddaa5481357
Quanlin Wan <quanlin.wan24@imperial.ac.uk>
2024-12-03 21:16:09 +0000

    Use Wrappping

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -16,16 +16,17 @@ public class ArrayDeque<Item> {
     private void resize(int capacity) {
         Item[] newLst = (Item[]) new Object[capacity];
         int newFirst = (int) Math.round(capacity * 0.25);
-        System.arraycopy(lst, 0, newLst, newFirst + 1, size());
+        for (int i = 0; i < size(); i++) {
+            newLst[newFirst + i] = lst[(nextFirst + 1 + i) % lst.length];
+            lst[(nextFirst + i + 1) % lst.length] = null;
+        }
         lst = newLst;
-        nextLast = newFirst + 1 + size();
-        nextFirst = newFirst;
+        nextLast = newFirst + size();
+        nextFirst = newFirst - 1;
     }
 
     private void checkUsage() {
-        if (nextFirst == -1) {
-            resize(lst.length * 2);
-        } else if (nextLast == lst.length) {
+        if ((nextFirst - 1 + lst.length) % lst.length == nextLast) {
             resize(lst.length * 2);
         } else if (size() < (lst.length) / 4 && size() > 8) {
             resize(lst.length / 2);
@@ -44,31 +45,30 @@ public class ArrayDeque<Item> {
         checkUsage();
         lst[nextFirst] = t;
         size += 1;
-        nextFirst -= 1;
+        nextFirst = (nextFirst - 1 + lst.length) % lst.length;
     }
 
     public void addLast(Item t) {
         checkUsage();
         lst[nextLast] = t;
         size += 1;
-        nextLast += 1;
+        nextLast = (nextLast + 1) % lst.length;
     }
 
     public Item get(int index) {
-        if (index >= 0 && index < size()){
-            return lst[index + nextFirst + 1];
-        } else {
+        if (isEmpty()) {
             return null;
         }
+        return lst[(nextFirst + 1 + index) % lst.length];
     }
 
     public Item removeFirst() {
         if (isEmpty()) {
             return null;
         }
-        Item t = lst[nextFirst + 1];
-        lst[nextFirst + 1] = null;
-        nextFirst += 1;
+        Item t = lst[(nextFirst + 1) % lst.length];
+        lst[(nextFirst + 1) % lst.length] = null;
+        nextFirst = (nextFirst + 1) % lst.length;
         size -= 1;
         return t;
     }
@@ -77,16 +77,16 @@ public class ArrayDeque<Item> {
         if (isEmpty()) {
             return null;
         }
-        Item t = lst[nextLast - 1];
-        lst[nextLast - 1] = null;
-        nextLast -= 1;
+        Item t = lst[(nextLast - 1 + lst.length) % lst.length];
+        lst[(nextLast - 1 + lst.length) % lst.length] = null;
+        nextLast = (nextLast - 1 + lst.length) % lst.length;
         size -= 1;
         return t;
     }
 
     public void printDeque() {
         for (int i = 0; i < size(); i++) {
-            System.out.print(lst[i] + " ");
+            System.out.print(lst[(nextFirst + 1 + i) % 12] + " ");
         }
         System.out.println(" ");
     }
