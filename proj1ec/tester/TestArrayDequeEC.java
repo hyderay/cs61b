a022929aee318cb646c0ff413c5252b2a72356ae
Quanlin Wan <quanlin.wan24@imperial.ac.uk>
2024-12-07 23:10:12 +0000

    fixed

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -1,49 +1,58 @@
 package tester;
 
 import static org.junit.Assert.*;
+
+import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
 import student.StudentArrayDeque;
-import edu.princeton.cs.introcs.StdRandom;
 
 public class TestArrayDequeEC {
+
     @Test
-    public void randomTest() {
+    public void randomizedTest() {
         ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
         StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
+        String msg = "";
 
         int N = 5000;
         for (int i = 0; i < N; i++) {
-            int random = StdRandom.uniform(4);
-            if (random == 0) {
-                int ranNum = StdRandom.uniform(100);
-                solution.addFirst(ranNum);
-                student.addFirst(ranNum);
-                int expected = solution.get(0);
-                int actual = student.get(0);
-                assertEquals("The first integer should be " + expected + ". But get "
-                                + actual, expected, actual);
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addFirst
+                int randVal = StdRandom.uniform(0, 100);
+                solution.addFirst(randVal);
+                student.addFirst(randVal);
 
-            } else if (random == 1) {
-                int ranNum = StdRandom.uniform(100);
-                solution.addLast(ranNum);
-                student.addLast(ranNum);
-                int expected = solution.get(solution.size() - 1);
-                int actual = student.get(student.size() - 1);
-                assertEquals("The last integer should be " + expected +
-                        ". But get " + actual, expected, actual);
+                msg += "addFirst(" + randVal + ")\n";
 
-            } else if (random == 2 && !solution.isEmpty() && !student.isEmpty()) {
+                assertEquals(msg, solution.get(0), student.get(0));
+            } else if (operationNumber == 1) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                solution.addLast(randVal);
+                student.addLast(randVal);
+
+                msg += "addLast(" + randVal + ")\n";
+
+                assertEquals(msg, solution.get(solution.size() - 1), student.get(student.size() - 1));
+            } else if (operationNumber == 2 && solution.size() > 0 && student.size() > 0) {
+                // removeFirst
                 Integer expected = solution.removeFirst();
                 Integer actual = student.removeFirst();
-                assertEquals(expected + " should be remove as the first one. But " +
-                        actual + " is removed", expected, actual);
 
-            } else if (random == 3 && !solution.isEmpty() && !student.isEmpty()) {
+                msg += "removeFirst()\n";
+
+                assertEquals(msg, expected, actual);
+            } else if (operationNumber == 3 && solution.size() > 0 && student.size() > 0) {
+                // removeLast
                 Integer expected = solution.removeLast();
                 Integer actual = student.removeLast();
-                assertEquals(expected + " should be remove as the last one. But " +
-                        actual + " is removed", expected, actual);
+
+                msg += "removeLast()\n";
+
+                assertEquals(msg, expected, actual);
             }
         }
+
     }
 }
