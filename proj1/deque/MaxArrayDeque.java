128d1c4d3f9c83182e9aa7d8609d02a93ee0460c
Quanlin Wan <quanlin.wan24@imperial.ac.uk>
2024-12-07 22:51:02 +0000

    Finished proj1ec

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -5,9 +5,26 @@ import java.util.Comparator;
 public class MaxArrayDeque<T> extends ArrayDeque<T> {
     private Comparator<T> t;
 
-    public MaxArrayDeque(Comparator<T> t) {
-        this.t = t;
+    public MaxArrayDeque(Comparator<T> c) {
+        super();
+        this.t = c;
     }
 
+    public T max() {
+        return max(t);
+    }
 
+    public T max(Comparator<T> c) {
+        if (this.isEmpty()) {
+            return null;
+        }
+        T maxE = this.get(0);
+        for (int i = 1; i < this.size(); i++) {
+            T pointer = this.get(i);
+            if (c.compare(pointer, maxE) > 0) {
+                maxE = pointer;
+            }
+        }
+        return maxE;
+    }
 }
---
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -0,0 +1,49 @@
+package tester;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import student.StudentArrayDeque;
+import edu.princeton.cs.introcs.StdRandom;
+
+public class TestArrayDequeEC {
+    @Test
+    public void randomTest() {
+        ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
+        StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
+
+        int N = 5000;
+        for (int i = 0; i < N; i++) {
+            int random = StdRandom.uniform(4);
+            if (random == 0) {
+                int ranNum = StdRandom.uniform(100);
+                solution.addFirst(ranNum);
+                student.addFirst(ranNum);
+                int expected = solution.get(0);
+                int actual = student.get(0);
+                assertEquals("The first integer should be " + expected + ". But get "
+                                + actual, expected, actual);
+
+            } else if (random == 1) {
+                int ranNum = StdRandom.uniform(100);
+                solution.addLast(ranNum);
+                student.addLast(ranNum);
+                int expected = solution.get(solution.size() - 1);
+                int actual = student.get(student.size() - 1);
+                assertEquals("The last integer should be " + expected +
+                        ". But get " + actual, expected, actual);
+
+            } else if (random == 2 && !solution.isEmpty() && !student.isEmpty()) {
+                int expected = solution.removeFirst();
+                int actual = student.removeFirst();
+                assertEquals(expected + " should be remove as the first one. But " +
+                        actual + " is removed", expected, actual);
+
+            } else if (random == 3 && !solution.isEmpty() && !student.isEmpty()) {
+                int expected = solution.removeLast();
+                int actual = student.removeLast();
+                assertEquals(expected + " should be remove as the last one. But " +
+                        actual + " is removed", expected, actual);
+            }
+        }
+    }
+}
