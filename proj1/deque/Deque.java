c135ce1539b335f2f5f24b0b053ba5dd06540033
Quanlin Wan <quanlin.wan24@imperial.ac.uk>
2024-12-10 19:54:03 +0000

    fixed

--- proj1/deque/Deque.java
+++ proj1/deque/Deque.java
@@ -1,18 +1,16 @@
 package deque;
 
-import java.util.Iterator;
-
 public interface Deque<T> {
-    public void addFirst(T item);
-    public void addLast(T item);
+     void addFirst(T item);
+     void addLast(T item);
 
-    default public boolean isEmpty() {
+    default boolean isEmpty() {
         return size() == 0;
     }
 
-    public int size();
-    public T get(int index);
-    public void printDeque();
-    public T removeFirst();
-    public T removeLast();
+    int size();
+    T get(int index);
+    void printDeque();
+    T removeFirst();
+    T removeLast();
 }
