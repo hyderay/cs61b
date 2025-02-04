d27c3ec90c0dc8446f4efa27b8471a4aca15b6d5
Sean W <wanquanlin@MacBook-Pro.local>
2025-02-04 23:30:53 +0000

    Added status method

--- proj2/gitlet/Log.java
+++ proj2/gitlet/Log.java
@@ -1,7 +1,13 @@
 package gitlet;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.TimeZone;
+
+import static gitlet.MyUtils.*;
+import static gitlet.Utils.*;
 
 public class Log {
     public static void execute() {
@@ -29,6 +35,38 @@ public class Log {
         String formattedDate = format.format(commit.getTimestamp());
         System.out.println("Date: " + formattedDate);
         System.out.println(commit.getMessage());
+        System.out.println();
+    }
+
+    public static void status() {
+
+        /** Print out branches. */
+        System.out.println("=== Branches ===");
+        File[] branches = Repository.getRefsDir().listFiles();
+        String headBranch = readContentsAsString(Repository.getHeadFile());
+        for (File branch : branches) {
+            String branchName = branch.getName();
+            if (branchName.equals(headBranch)) {
+                System.out.println("*" + branchName);
+            } else {
+                System.out.println(branchName);
+            }
+        }
+        System.out.println();
+
+        /** Print out staged files. */
+        System.out.println("=== Staged Files ===");
+        Staging stagingArea = new Staging();
+        for (String name : stagingArea.getStagedFiles().keySet()) {
+            System.out.println(name);
+        }
+        System.out.println();
+
+        /** Print out removed files. */
+        System.out.println("=== Removed Files ===");
+        for (String name : stagingArea.getRemovedFiles().keySet()) {
+            System.out.println(name);
+        }
         System.out.println();
     }
 }
