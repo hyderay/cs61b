81f3648fa5ad6ba1cdd889c55de7655820b32b9f
Sean W <wanquanlin@MacBook-Pro.local>
2025-02-05 00:42:20 +0000

    Implemented status supporting modification and untracked

--- proj2/gitlet/Log.java
+++ proj2/gitlet/Log.java
@@ -2,9 +2,7 @@ package gitlet;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.TimeZone;
+import java.util.List;
 
 import static gitlet.MyUtils.*;
 import static gitlet.Utils.*;
@@ -40,21 +38,21 @@ public class Log {
 
     public static void status() {
 
-        /** Print out branches. */
+        /** Print branches. */
         System.out.println("=== Branches ===");
         File[] branches = Repository.getRefsDir().listFiles();
         String headBranch = readContentsAsString(Repository.getHeadFile());
         for (File branch : branches) {
-            String branchName = branch.getName();
-            if (branchName.equals(headBranch)) {
-                System.out.println("*" + branchName);
+            String branchContent = readContentsAsString(branch);
+            if (branchContent.equals(headBranch)) {
+                System.out.println("*" + branch.getName());
             } else {
-                System.out.println(branchName);
+                System.out.println(branch.getName());
             }
         }
         System.out.println();
 
-        /** Print out staged files. */
+        /** Print staged files. */
         System.out.println("=== Staged Files ===");
         Staging stagingArea = new Staging();
         for (String name : stagingArea.getStagedFiles().keySet()) {
@@ -62,10 +60,38 @@ public class Log {
         }
         System.out.println();
 
-        /** Print out removed files. */
+        /** Print removed files. */
         System.out.println("=== Removed Files ===");
         for (String name : stagingArea.getRemovedFiles().keySet()) {
             System.out.println(name);
+        }
+        System.out.println();
+
+        /** Print modifications not staged files. */
+        System.out.println("=== Modifications Not Staged For Commit ===");
+        Commit headCommit = getHeadCommit();
+        for (String fileName : headCommit.getBlobFiles().keySet()) {
+            File file = new File(fileName);
+            if (!file.exists()) {
+                System.out.println(fileName + " (deleted)");
+            } else {
+                String currentContent = readContentsAsString(file);
+                String trackedFile = headCommit.getFileHash(fileName);
+                if (!sha1(currentContent).equals(trackedFile)) {
+                    System.out.println(fileName + " (modified)");
+                }
+            }
+        }
+        System.out.println();
+
+        /** Print untracked files. */
+        System.out.println("=== Untracked Files ===");
+        System.out.println("=== Untracked Files ===");
+        List<String> cwdFiles = plainFilenamesIn(Repository.CWD);
+        for (String file : cwdFiles) {
+            if (!stagingArea.getStagedFiles().containsKey(file) && headCommit.getFileHash(file) == null) {
+                System.out.println(file);
+            }
         }
         System.out.println();
     }
