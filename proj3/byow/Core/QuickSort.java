602e12fb62386d82acf2abd7bb2391e84199fa80
Sean W <wanquanlin@MacBook-Pro.local>
2025-02-24 21:27:30 +0000

    Added quick sort class

--- proj3/byow/Core/QuickSort.java
+++ proj3/byow/Core/QuickSort.java
@@ -1,4 +1,33 @@
 package byow.Core;
 
+import java.util.*;
+
 public class QuickSort {
+    public static void quickSort(List<Double> edges, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(edges, low, high);
+            quickSort(edges, low, pivotIndex - 1);
+            quickSort(edges, pivotIndex + 1, high);
+        }
+    }
+
+    private static int partition(List<Double> edges, int low, int high) {
+        double pivot = edges.get(low);
+        int left = low;
+        int right = high;
+
+        while (true) {
+            while (left <= high && edges.get(left) < pivot) {
+                left++;
+            }
+            while (right >= low && edges.get(right) > pivot) {
+                right--;
+            }
+            if (left >= right) {
+                return right;
+            }
+
+            Collections.swap(edges, left, right);
+        }
+    }
 }
