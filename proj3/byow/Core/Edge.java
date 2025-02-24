87422c7c824b948376a3c5ea7233455ca7c1dae2
Sean W <wanquanlin@MacBook-Pro.local>
2025-02-24 21:28:16 +0000

    Added helper classes

--- proj3/byow/Core/Edge.java
+++ proj3/byow/Core/Edge.java
@@ -1,4 +1,13 @@
 package byow.Core;
 
 public class Edge {
+    private double weight;
+
+    public Edge(double weight) {
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
 }
--- proj3/byow/Core/Room.java
+++ proj3/byow/Core/Room.java
@@ -1,4 +1,29 @@
 package byow.Core;
 
 public class Room {
+    private int x;
+    private int y;
+    private int centerX;
+    private int centerY;
+    private int width;
+    private int height;
+
+    public Room(int x, int y,
+                int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+
+        centerX = x + width / 2;
+        centerY = y + height / 2;
+    }
+
+    public int getCenterX() {
+        return centerX;
+    }
+
+    public int getCenterY() {
+        return centerY;
+    }
 }
